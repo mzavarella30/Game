65a6b954f8e714c5dbb5f2090eeb4abdaf31c3a6
Michael Zavarella <mzavarella30@gmail.com>
2015-08-13 21:38:36 -0400

    Video-10 Clearing the screen

--- src/com/Zav/Screen/Screen.java
+++ src/com/Zav/Screen/Screen.java
@@ -17,13 +17,21 @@ public class Screen
         pixels = new int[width * height];
     }
 
+    public void clear()
+    {
+        for (int i = 0; i < pixels.length; i++)
+        {
+            pixels[i] = 0;
+        }
+    }
+
     public void render()
     {
         for (int y = 0; y < height; y++)
         {
             for (int x = 0; x < width; x++)
             {
-                pixels[x+(y*width)] = 0xff00ff;
+                pixels[20+100 * width] = 0xff00ff;
             }
         }
     }
