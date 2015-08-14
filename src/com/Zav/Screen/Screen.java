7bfc9967a6708544705584b20ce4c5ccd4ddcb53
Michael Zavarella <mzavarella30@gmail.com>
2015-08-13 22:26:58 -0400

    Video-11 Creating a boundary

--- src/com/Zav/Screen/Screen.java
+++ src/com/Zav/Screen/Screen.java
@@ -10,14 +10,14 @@ public class Screen
     private int width, height;
     public int[] pixels;
 
-    int time = 0;
+    int xTime = 0, yTime = 0;
     int counter = 0;
 
     public Screen (int width, int height)
     {
         this.width = width;
         this.height = height;
-        pixels = new int[width * height];
+        pixels = new int[width * height]; //50,400 elements in the pixels array
     }
 
     public void clear()
@@ -31,15 +31,16 @@ public class Screen
     public void render()
     {
         counter++;
-        if (counter % 100 == 0)
-        {
-            time++;
-        }
+        if (counter % 2 == 0) xTime++;
+        if (counter % 20 == 0) yTime++;
+
         for (int y = 0; y < height; y++)
         {
+            if (yTime >= height) break;
             for (int x = 0; x < width; x++)
             {
-                pixels[time + time * width] = 0xff00ff;
+                if (xTime >= width) break;
+                pixels[xTime + yTime * width] = 0xff00ff;
             }
         }
     }
