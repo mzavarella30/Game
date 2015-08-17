b39a283434323b0e80aeaddd8a319f844fba2338
Michael Zavarella <mzavarella30@gmail.com>
2015-08-17 07:49:03 -0400

    Had to re-do my random level class

--- src/com/Zav/Game/level/RandomLevel.java
+++ src/com/Zav/Game/level/RandomLevel.java
@@ -1,29 +1,25 @@
-/**
- * Created by Michael Zavarella
- */
 package com.Zav.Game.Level;
 
+import com.Zav.Game.Level.Level;
 import java.util.Random;
 
 public class RandomLevel extends Level
 {
     private final Random random = new Random();
 
-    public RandomLevel(int width, int height)
+    public RandomLevel (int width, int height)
     {
         super(width, height);
     }
 
-    @Override
     protected void generateLevel()
     {
-        for (int y = 0; y < height; y++)
+        for(int y = 0; y < height; y++)
         {
-            for (int x = 0; x < width; x++)
+            for(int x = 0; x < width; x++)
             {
-                tiles[x + y * width] = random.nextInt(4);
+                tiles[x+y*width] = random.nextInt(4);
             }
         }
-
     }
 }
