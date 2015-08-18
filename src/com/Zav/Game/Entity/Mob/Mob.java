a860a247d0ead18862fc2f9f94b2a93817f2d439
Michael Zavarella <mzavarella30@gmail.com>
2015-08-18 19:23:57 -0400

    Video 42 The meaning of life, movement

--- src/com/Zav/Game/Entity/Mob/Mob.java
+++ src/com/Zav/Game/Entity/Mob/Mob.java
@@ -11,7 +11,16 @@ public abstract class Mob extends Entity {
     protected int dir = 0;
     protected boolean moving = false;
 
-    public void move() {
+    public void move(int xa, int ya) {
+        if (xa > 0) dir = 1; // East
+        if (xa < 0) dir = 3; // West
+        if (ya > 0) dir = 2; // South
+        if (ya < 0) dir = 0; // North
+
+        if (!collision()) {
+            x += xa;
+            y += ya;
+        }
     }
 
     public void update() {
