a14610a5025c9d8b9c0c4f37c7f4957f63ab2b91
Michael Zavarella <mzavarella@wingspan.com>
2015-08-18 16:37:03 -0400

    Video 35 complete

--- src/com/Zav/Game/level/Level.java
+++ src/com/Zav/Game/level/Level.java
@@ -39,6 +39,14 @@ public class Level
         int x1 = (xScroll + screen.width) >> 4;
         int y0 = yScroll >> 4;
         int y1 = (yScroll + screen.height) >> 4;
+
+        for (int y = y0; y < y1; y++)
+        {
+            for(int x = x0; x < x1; x++)
+            {
+                getTile(x, y).render(x, y, screen);
+            }
+        }
     }
 
     public Tile getTile(int x, int y)
