4fed41eeb67628d35a7de62a7a6648a3b0d438d0
Michael Zavarella <mzavarella30@gmail.com>
2015-08-18 19:07:50 -0400

    Video 40 Mob class

---
+++ src/com/Zav/Game/Entity/Mob/Mob.java
@@ -0,0 +1,25 @@
+/**
+ * Created by Michael Zavarella
+ */
+package com.Zav.Game.Entity.Mob;
+
+import com.Zav.Game.Entity.Entity;
+import com.Zav.Game.Graphics.Sprite;
+
+public abstract class Mob extends Entity
+{
+    protected Sprite sprite;
+    protected int dir = 0;
+    protected boolean moving = false;
+
+    public void move()
+    {}
+
+    public void update()
+    {}
+
+    private boolean collision()
+    {
+        return false;
+    }
+}
